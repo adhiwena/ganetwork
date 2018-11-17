886f912a471863c7fc96b0d3b05620b537e265d2
Aji A. Andoyo <adhiwena@gmail.com>
2018-11-17 08:31:05 +0700

    Update GA Network Steam Button.user.js

--- GA Network Steam Button.user.js
+++ GA Network Steam Button.user.js
@@ -2,9 +2,9 @@
 // @name         GA Network Steam Button
 // @icon         http://store.giveawaynetwork.xyz/shop/dist/img/favicon.ico
 // @namespace    http://giveawaynetwork.xyz/
-// @version      0.3
+// @version      0.4
 // @description  what
-// @homepage 	https://github.com/adhiwena/tmprmnky
+// @homepage 	https://github.com/adhiwena/ganetwork
 // @updateURL 	https://raw.githubusercontent.com/adhiwena/ganetwork/master/GA Network Steam Button.user.js
 // @downloadURL https://raw.githubusercontent.com/adhiwena/ganetwork/master/GA Network Steam Button.user.js
 // @author       cyp
@@ -16,12 +16,12 @@
 
 (function() {
     $('.buy_button').each(function() {
+        let barterlnk = $(this).parent().parent().find('th:first').text();
+        $(this).parent().append('<a class="btn btn-danger barterLink" style="margin:2px;" href="https://barter.vg/search?q='+barterlnk+'" target="_blank">'+'Barter</a>');
+        
         let str = $(this).attr('href');
         let steamlnk = 'https://store.steampowered.com/app/' + str.substring(6, 12);
         $(this).parent().append('<a class="btn btn-primary steamLink" style="margin:2px;" href="'+steamlnk+'" target="_blank">'+'Steam</a>');
-    });
-    $('.table-hover tbody tr th:first-child').each(function() {
-        let barterlnk = $(this).text();
-        $(this).append('<a style="margin:2px;" href="https://barter.vg/search?q='+barterlnk+'" target="_blank">(Barter)</a>');
+
     });
 })();
