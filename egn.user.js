31da3f8cd5a6dd9504a770a2f198e818ea92786a
adhiwena <adhiwena@gmail.com>
2019-04-10 00:54:03 +0700

    Update Compact Search

--- egn.user.js
+++ egn.user.js
@@ -184,34 +184,42 @@
         //end compre
     }
 
-        function sortTable(table, col, reverse) {
-            var tb = table.tBodies[0],
-                tr = Array.prototype.slice.call(tb.rows, 0),
-                i;
-            reverse = -((+reverse) || -1);
-            tr = tr.sort(function (a, b) {
-                return reverse
-                    * (a.cells[col].textContent.trim()
-                       .localeCompare(b.cells[col].textContent.trim(), undefined, {numeric: true})
-                      );
-            });
-            for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
-        }
+    function sortTable(table, col, reverse) {
+        var tb = table.tBodies[0],
+            tr = Array.prototype.slice.call(tb.rows, 0),
+            i;
+        reverse = -((+reverse) || -1);
+        tr = tr.sort(function (a, b) {
+            return reverse
+                * (a.cells[col].textContent.trim()
+                   .localeCompare(b.cells[col].textContent.trim(), undefined, {numeric: true})
+                  );
+        });
+        for(i = 0; i < tr.length; ++i) tb.appendChild(tr[i]);
+    }
 
-        function makeSortable(table) {
-            var th = table.tHead, i;
-            th && (th = th.rows[0]) && (th = th.cells);
-            if (th) i = th.length;
-            else return;
-            while (--i >= 0) (function (i) {
-                var dir = 1;
-                th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
-            }(i));
-        }
+    function makeSortable(table) {
+        var th = table.tHead, i;
+        th && (th = th.rows[0]) && (th = th.cells);
+        if (th) i = th.length;
+        else return;
+        while (--i >= 0) (function (i) {
+            var dir = 1;
+            th[i].addEventListener('click', function () {sortTable(table, i, (dir = 1 - dir))});
+        }(i));
+    }
 
-        function makeAllSortable(parent) {
-            parent = parent || document.body;
-            var t = parent.getElementsByTagName('table'), i = t.length;
-            while (--i >= 0) makeSortable(t[i]);
-        }
+    function makeAllSortable(parent) {
+        parent = parent || document.body;
+        var t = parent.getElementsByTagName('table'), i = t.length;
+        while (--i >= 0) makeSortable(t[i]);
+    }
+
+    $('body').on('DOMNodeInserted', '.select2-results li', function () {
+          $(this).parent().addClass('list-group');
+          $(this).addClass('list-group-item');
+          $(this).find('.select2-result-game__img').attr('style','display:none');
+          $(this).find('.select2-result-game__title').attr('style', 'font-size: inherit');
+          $(this).find('.select2-result-game__price').attr('style', 'text-align: right');
+    });
 })();
